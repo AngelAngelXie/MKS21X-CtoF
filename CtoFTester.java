639387e06128b6842e04b2f4faa9c4374bfb3c24
JiaXin Xie <jxie21@stuy.edu>
2021-09-23 20:20:00 -0400

    initial skeleton

---
+++ CtoFTester.java
@@ -0,0 +1,21 @@
+/** What should the functions have as parameters? What type?
+ *  Celcius double
+ *  Fahrenheit double
+ *  What should the functions return? 
+ *  Converted temperature in double.
+ */ 
+ 
+public class CtoFTester {
+    public double celsiusToFahrenheit(){
+        return 0.0;
+    }
+
+    public double fahrenheitToCelsius(){
+        return 0.0;
+    }
+    
+    public static void main(String[] args){
+
+    }
+
+}
