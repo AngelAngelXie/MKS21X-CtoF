a00b2654ef62e6be639cd6e406d5af4c3fb64a16
JiaXin Xie <jxie21@stuy.edu>
2021-09-23 22:04:06 -0400

    completed fahrenheitToCelsius

--- CtoFTester.java
+++ CtoFTester.java
@@ -10,12 +10,15 @@ public class CtoFTester {
         System.out.print(celsius + " C is approximiately equivalent to " + (9.0 / 5.0 * celsius + 32.0) + " F."); 
     }
 
-    static void fahrenheitToCelsius(){
-        System.out.print("underconstruction");
+    static void fahrenheitToCelsius(double fahrenheit){
+        System.out.print(fahrenheit + " F is approximately equivalent to " + ((fahrenheit - 32) * 5 / 9) + " C.");
     }
     
     public static void main(String[] args){
         celsiusToFahrenheit(30.0);
+        System.out.println("\n");
+        fahrenheitToCelsius(100.0);
+
     }
 
 }
