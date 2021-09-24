a27c9b4536a337a1b52e6668cb98ef54b5b99b23
JiaXin Xie <jxie21@stuy.edu>
2021-09-23 21:57:05 -0400

    re-commit: fixed problems in skeleton + completed celsiusToFahrenheit

--- CtoFTester.java
+++ CtoFTester.java
@@ -1,21 +1,21 @@
 /** What should the functions have as parameters? What type?
- *  Celcius double
- *  Fahrenheit double
+ *  - Celcius double
+ *  - Fahrenheit double
  *  What should the functions return? 
- *  Converted temperature in double.
+ *  - [input] is approximately equivalent to [output].
  */ 
  
 public class CtoFTester {
-    public double celsiusToFahrenheit(){
-        return 0.0;
+    static void celsiusToFahrenheit(double celsius){
+        System.out.print(celsius + " C is approximiately equivalent to " + (9.0 / 5.0 * celsius + 32.0) + " F."); 
     }
 
-    public double fahrenheitToCelsius(){
-        return 0.0;
+    static void fahrenheitToCelsius(){
+        System.out.print("underconstruction");
     }
     
     public static void main(String[] args){
-
+        celsiusToFahrenheit(30.0);
     }
 
 }
